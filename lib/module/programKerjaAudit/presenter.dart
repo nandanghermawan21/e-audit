f986e4ef99f7989df108259200f275b30daf167e
nandanghermawan <nandang@enerren.com>
2023-08-06 23:54:14 +0700

    commit

---
+++ lib/module/programKerjaAudit/presenter.dart
@@ -0,0 +1,24 @@
+import 'package:eaudit/model/program_kerja_audit_model.dart';
+import 'package:flutter/material.dart';
+import '../../component/list_data_component.dart';
+import 'view.dart';
+
+class Presenter extends StatefulWidget {
+  final State<Presenter>? view;
+
+  const Presenter({
+    Key? key,
+    this.view,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: no_logic_in_create_state
+    return view ?? View();
+  }
+}
+
+abstract class PresenterState extends State<Presenter> {
+  ListDataComponentController<ProgramKerjaAuditModel> listController =
+      ListDataComponentController<ProgramKerjaAuditModel>();
+}
