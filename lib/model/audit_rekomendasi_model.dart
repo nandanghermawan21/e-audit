60cef8bbe437fbbfc81bef3cadade1d53e8457f0
Nandang Hermawan <nadanghermawan2014@gmail.com>
2023-08-25 23:36:47 +0700

    add rekomendasi status color and legend

--- lib/model/audit_rekomendasi_model.dart
+++ lib/model/audit_rekomendasi_model.dart
@@ -6,14 +6,17 @@ class AuditRekomendasiModel {
   String? deskripsi;
   String? status;
   String? statusRekomensai;
-  String? sisaHariTindakLanjut;
   Color? color;
+  Color? statusRekomendasiColor;
+  String? sisaHariTindakLanjut;
   List<AuditTLItemModel?>? listItem;
   List<KomentarModel?>? komentar;
 
   AuditRekomendasiModel({
     this.deskripsi,
     this.status,
+    this.statusRekomensai,
+    this.statusRekomendasiColor,
     this.sisaHariTindakLanjut,
     this.color,
     this.listItem,
@@ -24,6 +27,10 @@ class AuditRekomendasiModel {
     return AuditRekomendasiModel(
       deskripsi: json["deskripsi"],
       status: json["status"],
+      statusRekomensai: json["status_rekomendasi"],
+      statusRekomendasiColor: json["status_rekomendasi_color"] != null
+          ? Color(json["status_rekomendasi_color"])
+          : null,
       sisaHariTindakLanjut: json["sisa_hari_tindak_lanjut"],
       color: json["color"] != null ? Color(json["color"]) : null,
       listItem: json["list_item"] != null
@@ -43,6 +50,8 @@ class AuditRekomendasiModel {
     return {
       "deskripsi": deskripsi,
       "status": status,
+      "status_rekomendasi": statusRekomensai,
+      "status_rekomendasi_color": statusRekomendasiColor?.value,
       "sisa_hari_tindak_lanjut": sisaHariTindakLanjut,
       "color": color?.value,
       "komentar": komentar?.map((e) => e?.toJson()).toList(),
@@ -57,6 +66,8 @@ class AuditRekomendasiModel {
         deskripsi:
             "Pemimpin Cabang Serang agar memberikan instruksi tertulis kepada Manajer Bisnis KC Serang untuk mempedomani SLA Penerbitan SP KBG pada setiap pengajuan proses penjaminan.",
         status: "Selesai",
+        statusRekomensai: "Belum Tindak Lanjut",
+        statusRekomendasiColor: Colors.red,
         color: Colors.green,
         sisaHariTindakLanjut: "10",
         komentar: KomentarModel.dummy(),
@@ -66,8 +77,43 @@ class AuditRekomendasiModel {
         deskripsi:
             "Untuk selanjutnya, Pemimpin Cabang KC Serang agar lebih memperhatikan SLA dalam menerbitkan SP Kontra Bank Garansi.",
         status: "Belum Tindak Lanjut",
+        statusRekomensai: "Dalam Proses Reviu Auditor",
+        statusRekomendasiColor: Colors.blue,
         sisaHariTindakLanjut: "10",
         color: Colors.orange,
+        komentar: KomentarModel.dummy(),
+        listItem: AuditTLItemModel.dummy(),
+      ),
+      AuditRekomendasiModel(
+        deskripsi:
+            "Pemimpin Cabang Serang agar memberikan instruksi tertulis kepada Manajer Bisnis KC Serang untuk mempedomani SLA Penerbitan SP KBG pada setiap pengajuan proses penjaminan.",
+        status: "Selesai",
+        statusRekomensai: "Dalam Proses Reviu Manager",
+        statusRekomendasiColor: Colors.orange,
+        color: Colors.green,
+        sisaHariTindakLanjut: "10",
+        komentar: KomentarModel.dummy(),
+        listItem: AuditTLItemModel.dummy(),
+      ),
+      AuditRekomendasiModel(
+        deskripsi:
+            "Untuk selanjutnya, Pemimpin Cabang KC Serang agar lebih memperhatikan SLA dalam menerbitkan SP Kontra Bank Garansi.",
+        status: "Belum Tindak Lanjut",
+        statusRekomensai: "Disetujui Manager Reviu Staff QA",
+        statusRekomendasiColor: Colors.purple,
+        sisaHariTindakLanjut: "10",
+        color: Colors.orange,
+        komentar: KomentarModel.dummy(),
+        listItem: AuditTLItemModel.dummy(),
+      ),
+      AuditRekomendasiModel(
+        deskripsi:
+            "Pemimpin Cabang Serang agar memberikan instruksi tertulis kepada Manajer Bisnis KC Serang untuk mempedomani SLA Penerbitan SP KBG pada setiap pengajuan proses penjaminan.",
+        status: "Selesai",
+        statusRekomensai: "Disetujui Staff QA Reviu Kabag QA",
+        statusRekomendasiColor: Colors.green,
+        color: Colors.green,
+        sisaHariTindakLanjut: "10",
         komentar: KomentarModel.dummy(),
         listItem: AuditTLItemModel.dummy(),
       ),
