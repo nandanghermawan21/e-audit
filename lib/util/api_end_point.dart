04028faf08c7d071a97f8ced61420dc531e198f7
nandanghermawan <nandang@enerren.com>
2023-10-10 10:28:25 +0700

    commit

--- lib/util/api_end_point.dart
+++ lib/util/api_end_point.dart
@@ -1,7 +1,7 @@
 import 'package:eaudit/util/mode_util.dart';
 
 class ApiEndPoint {
-  String baseUrl = "https://dev-jamkrindo.banggasolution.com/Api/";
+  String baseUrl = "https://jamkrindo.banggasolution.com/Api/";
   String baseUrlDebug = "https://jamkrindo.banggasolution.com/Api/";
   // String baseUrl = "https://form.bagdja.com/api/";
   // String baseUrlDebug = "https://form.bagdja.com/api/";
